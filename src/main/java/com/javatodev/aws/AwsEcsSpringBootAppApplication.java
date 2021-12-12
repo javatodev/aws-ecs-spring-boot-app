ff69045b00b6411eecd2b0c50fffbd7ef8565430
javatodev <sendtospringboot@gmail.com>
2021-12-12 23:42:52 +0530

    init

---
+++ src/main/java/com/javatodev/aws/AwsEcsSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsEcsSpringBootAppApplication.class, args);
+    }
+
+}
